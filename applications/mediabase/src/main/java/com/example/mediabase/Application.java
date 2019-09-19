03ee5d9cc43efd8d71f30794561acc95028a9b29
Zezhi Xia <zezhi.xia@perficient.com>
2019-09-19 15:05:11 -0400

    finish lab6

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import com.example.mediabase.moviesui.MovieClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class Application {
 
     public static void main(String... args) {
--- components/ui/src/main/java/com/example/mediabase/podcastsui/PodcastClient.java
+++ components/ui/src/main/java/com/example/mediabase/podcastsui/PodcastClient.java
@@ -1,16 +1,23 @@
 package com.example.mediabase.podcastsui;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestOperations;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PodcastClient {
-    private static ParameterizedTypeReference<List<PodcastUI>> movieListType = new ParameterizedTypeReference<List<PodcastUI>>() {
+    private static ParameterizedTypeReference<List<PodcastUI>> podcastListType = new ParameterizedTypeReference<List<PodcastUI>>() {
     };
     private RestOperations restOperations;
     private String podcastsURL;
+    private static final int CACHE_SIZE = 5;
+    private final List<PodcastUI> lastRead = new ArrayList<>(CACHE_SIZE);
+    private static final Logger log = LoggerFactory.getLogger(PodcastClient.class);
 
 
     public PodcastClient(String moviesURL, RestOperations restOperations) {
@@ -22,8 +29,24 @@ public class PodcastClient {
         restOperations.postForEntity(podcastsURL, podcast, PodcastUI.class);
     }
 
+    @HystrixCommand(fallbackMethod="findAllFallback")
     public List<PodcastUI> findAll() {
-        return restOperations.exchange(podcastsURL, HttpMethod.GET, null, movieListType).getBody();
+        List<PodcastUI> read = restOperations.exchange(podcastsURL, HttpMethod.GET, null, podcastListType).getBody();
+        log.debug("Read {} podcasts from {}", read.size(), podcastsURL);
+
+        lastRead.clear();
+        int copyCount = (read.size() < CACHE_SIZE) ? read.size() : CACHE_SIZE;
+        for (int i =0; i < copyCount; i++)
+            lastRead.add(read.get(i));
+        log.debug("Copied {} podcasts into the cache", copyCount);
+
+        return read;
+    }
+
+    public List<PodcastUI> findAllFallback() {
+        log.debug("Returning {} podcasts from the fallback method", lastRead.size());
+
+        return lastRead;
     }
 
 
