a772895a1a8f76b6415905f9b028f363b42143a5
Zezhi Xia <zezhi.xia@perficient.com>
2019-09-19 17:12:02 -0400

    small change

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -28,11 +28,6 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
-    @Bean
-    @LoadBalanced
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
